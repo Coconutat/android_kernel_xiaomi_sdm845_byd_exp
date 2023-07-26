a8853c91c69f30a104893331c06e00bf435dca4c
Coconut <coconutat@protonmail.com>
2023-07-26 15:03:20 +0800

    Fix AnyKernel 3 for Mi8

---
+++ tools/AnyKernel3_sh/anykernel.sh
@@ -0,0 +1,37 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+properties() { '
+kernel.string=Xiaomi Mi 8 UD Kernel Mod For MIUI 12.5.x Version -- Kernel by Coconutat_Ian @ Github
+do.devicecheck=1
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=equuleus
+supported.versions=
+supported.patchlevels=
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+
+## boot shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
