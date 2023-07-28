3c6d047585104cc8c35d3dde012ea4a9ea46fc60
Coconut <coconutat@protonmail.com>
2023-07-28 12:01:17 +0800

    KernelSU:Fix modify Open.c  not use C99 stand

--- fs/open.c
+++ fs/open.c
@@ -369,7 +369,6 @@ extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int
 
 SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 {
-	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 	const struct cred *old_cred;
 	struct cred *override_cred;
 	struct path path;
@@ -377,6 +376,8 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
 
 	if (mode & ~S_IRWXO)	/* where's F_OK, X_OK, W_OK, R_OK? */
 		return -EINVAL;
